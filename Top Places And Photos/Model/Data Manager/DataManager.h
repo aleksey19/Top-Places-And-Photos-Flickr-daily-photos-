509552a1a71ad13152b9ddd91a4d546aea80fca3
Aleksey <Aleksey@Alekseys-Mac.local>
2016-06-17 20:01:16 +0300

    Added db manager

---
+++ Top Places And Photos/Model/Data Manager/DataManager.h
@@ -0,0 +1,13 @@
+//
+//  DataManager.h
+//  Top Places And Photos
+//
+//  Created by Aleksey on 6/17/16.
+//  Copyright © 2016 Aleksey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataManager : NSObject
+
+@end
---
+++ Top Places And Photos/Model/Data Manager/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  Top Places And Photos
+//
+//  Created by Aleksey on 6/17/16.
+//  Copyright © 2016 Aleksey. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
